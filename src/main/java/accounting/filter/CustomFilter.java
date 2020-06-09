38d9bbe4670398b12ca3fd38f6c18113b195895a
TanskiV <tanskiyv@gmail.com>
2020-06-09 13:49:22 +0300

    v1.3 filter by id

--- src/main/java/accounting/filter/CustomFilter.java
+++ src/main/java/accounting/filter/CustomFilter.java
@@ -1,13 +1,14 @@
 package accounting.filter;
 
-import accounting.exeptions.TokenAuthenticationException;
 import accounting.jwt.TokenProvider;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.servlet.HandlerMapping;
 
 import javax.naming.AuthenticationException;
 import javax.servlet.FilterChain;
@@ -16,7 +17,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.server.PathParam;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -30,9 +34,11 @@ public class CustomFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         String token = req.getHeader("X-Token");
+        String idFromPath = req.getServletPath().split("/")[4];
+        Claims claimsToken = tokenProvider.decodeJWT(token);
         if (!req.getMethod().equals(RequestMethod.POST.toString())) {
             try {
-                if (tokenProvider.validateToken(token)) {
+                if (tokenProvider.validateToken(token) && idFromPath.equals(claimsToken.getId())) {
                     chain.doFilter(request, response);
                 } else {
                     ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, "Token not a valid");
